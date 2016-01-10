49f8c0d602b6164f51192b0a2446e9034a17f4ec
puretears <nacci520@gmail.com>
2016-01-10 12:57:09 +0800

    update optional variable

--- dive-into-swift/arc2.playground/Contents.swift
+++ dive-into-swift/arc2.playground/Contents.swift
@@ -22,7 +22,7 @@ class Person {
 class Apartment {
     let unit: String
     weak var tenant: Person?
-    let owner: Person
+    unowned let owner: Person
     
     init(unit: String, owner: Person) {
         self.unit = unit
@@ -36,7 +36,7 @@ class Apartment {
 }
 
 var mars: Person? = Person(name: "Mars")
-var apt11: Apartment? = Apartment(unit: "11")
+var apt11: Apartment? = Apartment(unit: "11", owner: mars!)
 
 mars!.apartment = apt11
 apt11!.tenant = mars
@@ -48,17 +48,29 @@ apt11 = nil
 
 //: #### unowned reference
 
+class Country {
+    let name: String
+    // Implicitly Unwrapped Optional
+    var capital: City! // default to nil
+    
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capital = City(name: capitalName, country: self)
+    }
+}
 
+class City {
+    let name: String
+    unowned let country: Country
+    
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
 
-
-
-
-
-
-
-
-
-
+var cn: Country? = Country(name: "China", capitalName: "Beijing")
+var bj: City? = City(name: "Beijing", country: cn!)
 
 
 
