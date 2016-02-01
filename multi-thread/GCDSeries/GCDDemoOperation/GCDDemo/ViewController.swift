817a346583b0114233a435ca31de3771eae0813b
puretears <nacci520@gmail.com>
2016-02-01 21:33:56 +0800

    operation queue demo

--- multi-thread/GCDSeries/GCDDemoOperation/GCDDemo/ViewController.swift
+++ multi-thread/GCDSeries/GCDDemoOperation/GCDDemo/ViewController.swift
@@ -41,42 +41,66 @@ class ViewController: UIViewController {
         NSURLCache.sharedURLCache().removeAllCachedResponses()
     }
 
+    let queue = NSOperationQueue()
+    
+    @IBAction func cancelDownload(sender: AnyObject) {
+        self.queue.cancelAllOperations()
+    }
     @IBAction func downloadImages(sender: UIButton) {
-        var currQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
-        currQueue = dispatch_queue_create("com.boxueio.images", DISPATCH_QUEUE_CONCURRENT)
         
-        // dispatch_async
-        dispatch_async(currQueue, {
+        queue.addOperationWithBlock({
             let img1 = Downloader.downloadImageWithURL(self.imageUrls[0])
-            dispatch_async(dispatch_get_main_queue(), {
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.image1.image = img1
                 self.image1.clipsToBounds = true
             })
         })
         
-        dispatch_async(currQueue, {
+        let op2 = NSBlockOperation(block: {
             let img2 = Downloader.downloadImageWithURL(self.imageUrls[1])
-            dispatch_async(dispatch_get_main_queue(), {
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.image2.image = img2
                 self.image2.clipsToBounds = true
             })
         })
         
-        dispatch_async(currQueue, {
+        op2.completionBlock = { print("image2 downloaded") }
+        
+//        queue.addOperation(op2)
+        
+        let op3 = NSBlockOperation(block: {
             let img3 = Downloader.downloadImageWithURL(self.imageUrls[2])
-            dispatch_async(dispatch_get_main_queue(), {
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.image3.image = img3
                 self.image3.clipsToBounds = true
             })
         })
         
-        dispatch_async(currQueue, {
+        op3.completionBlock = { print("image3 is cancelled: \(op3.cancelled)") }
+        
+//        queue.addOperation(op3)
+        
+        let op4 = NSBlockOperation(block: {
             let img4 = Downloader.downloadImageWithURL(self.imageUrls[3])
-            dispatch_async(dispatch_get_main_queue(), {
+            
+            NSOperationQueue.mainQueue().addOperationWithBlock({
                 self.image4.image = img4
                 self.image4.clipsToBounds = true
             })
         })
+        
+        op4.completionBlock = { print("image4 downloaded") }
+        
+        queue.addOperation(op4)
+        
+        op3.addDependency(op4)
+        op2.addDependency(op3)
+        
+        queue.addOperation(op3)
+        queue.addOperation(op2)
     }
 }
 
