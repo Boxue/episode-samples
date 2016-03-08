28f8fc04a8ae4c08783da9b5cc0b1e34d972db9e
puretears <nacci520@gmail.com>
2016-03-08 14:41:58 +0800

    encode demo update

--- alamofire-tutorials/AlamoFireURLEncoding/AlamoFireDemo/ViewController.swift
+++ alamofire-tutorials/AlamoFireURLEncoding/AlamoFireDemo/ViewController.swift
@@ -10,33 +10,48 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-//        let requestUrl = "https://apidemo.boxue.io/alamofire?XDEBUG_SESSION_START=PHPSTORM"
-//        let noteUrl = "https://apidemo.boxue.io/alamofire/note-list?XDEBUG_SESSION_START=PHPSTORM"
-        
-        let requestUrl = "https://apidemo.boxue.io/alamofire?XDEBUG_SESSION_START=PHPSTORM"
-        
+
+        var requestUrl =
+        "https://apidemo.boxue.io/alamofire?XDEBUG_SESSION_START=PHPSTORM"
+        // Define a simple KV parameter
         let parameters = [
-            "foo": [1, 2],
-            "bar": [
-                "x": "a",
-                "y": "2"
-            ]
+                "foo": [1, 2],
+                "bar": [
+                        "x": "a",
+                        "y": 2
+                ]
         ]
-        
-        Alamofire.request(.GET, requestUrl, parameters: parameters)
-            .responseString(completionHandler: { response in
-                switch response.result {
-                case .Success(let json):
-                    print("JSON: ================")
-                    print("\(json)")
-                case .Failure(let error):
-                    print("\(error)")
-                }
-            })
+
+        // Parameters encoding in URL
+        // [ "foo": [1, 2]]
+        // [ "bar": ["x": "a", "y": 2]]
+//        let requestParameters = "&foo[]=1&foo[]=2&bar[x]=a&bar[y]=2"
+//        requestUrl = requestUrl + requestParameters
+//        requestUrl.stringByAddingPercentEncodingWithAllowedCharacters(
+//            NSCharacterSet.URLQueryAllowedCharacterSet()
+//        )
+        // .URL
+        // HEAD / GET / DELETE - Put encoded parameter in request uri
+        // Others - in HTTP body
+        // .URLEncodedInURL
+        // Always put encoded parameters in request url
+        Alamofire.request(.POST,
+                requestUrl,
+                parameters: parameters,
+                encoding: .JSON)
+//        Alamofire.request(.GET, requestUrl)
+        .responseJSON(completionHandler: { response in
+            switch response.result {
+            case .Success(let json):
+                print("JSON: ================")
+                print("\(json)")
+            case .Failure(let error):
+                print("\(error)")
+            }
+        })
 
 
     }
