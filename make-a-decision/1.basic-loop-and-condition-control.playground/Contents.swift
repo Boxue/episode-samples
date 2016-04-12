caed3575672f38ddf9a4e00dbf00ea7aceb18baa
puretears <nacci520@gmail.com>
2016-04-12 22:53:10 +0800

    update for swift 3

--- make-a-decision/1.basic-loop-and-condition-control.playground/Contents.swift
+++ make-a-decision/1.basic-loop-and-condition-control.playground/Contents.swift
@@ -9,7 +9,7 @@ import UIKit
 
 //: for 
 
-for var i = 0; i < 10; i++ {
+for var i = 0; i < 10; i += 1 {
     print(i)
 }
 
@@ -33,7 +33,7 @@ for _ in 1...10 {
 var i  = 0
 while i < 10 {
     print(i)
-    ++i
+    i += 1
 }
 
 // do ... while
@@ -41,7 +41,7 @@ var n  = 0
 
 repeat {
     print(vowel[n])
-    ++n
+    n += 1
 } while n < 5
 
 //: if
@@ -57,5 +57,3 @@ else if PI == 3.1415926 {
 else {
 
 }
-
-var person: String?
