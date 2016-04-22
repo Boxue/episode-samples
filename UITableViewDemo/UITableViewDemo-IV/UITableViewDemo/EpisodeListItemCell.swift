76b9ed95b813aba8c7f59f98a78fc343d213aeae
puretears <nacci520@gmail.com>
2016-04-22 18:16:12 +0800

    UIStackView demo

---
+++ UITableViewDemo/UITableViewDemo-IV/UITableViewDemo/EpisodeListItemCell.swift
@@ -0,0 +1,12 @@
+//
+//  EpisodeListItemCell.swift
+//  UITableViewDemo
+//
+//  Created by Mars on 4/19/16.
+//  Copyright © 2016 Boxue. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeListItemCell: UITableViewCell {
+}
